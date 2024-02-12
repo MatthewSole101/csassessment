8b7b89a70035421b95cc71b880f1c66f4842c738
MatthewSole101 <91541357+MatthewSole101@users.noreply.github.com>
2024-02-12 18:44:04 +0000

    widk

---
+++ assesment1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace assesment1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+    
+    public class Die
+    {
+        private int[] _Rolls = { 1, 2, 3, 4, 5, 6 };
+        private int _Roll;
+        
+
+        public int Rolls
+        {
+            get { return _Rolls;}
+            set { Rolls = value; }
+        }
+        public int Roll
+        {
+            get { return _Roll;}
+            set { Roll = value; }
+        }
+
+
+        static void Rolling
+        {
+            num = rand.Next(5);
+            return _Rolls[num]
+        }
+        
+}
